0539a09294bac479df6203c0d95d7689ef291b0c
David Vesel <dawe.vesely@gmail.com>
2019-04-14 20:34:37 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const fileInput = document.querySelector('input[type="file"]');
+const result = document.querySelector('.result');
+
+function download(filename, text) {
+  result.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  result.setAttribute('download', filename);
+  result.innerHTML = `Download <span>${filename}</span>`;
+
+  element.click();
+}
+
+fileInput.addEventListener('change', (event) => {
+  event.preventDefault();
+
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+
+  reader.readAsText(file, "windows-1250"); // TODO: auto detect encoding
+
+  reader.onload = (event) => {
+    console.log(fileInput.files[0]);
+    download(file.name, event.target.result);
+  };
+
+  reader.onerror = (event) => {
+    console.error(event);
+    alert('Error');
+  };
+});
